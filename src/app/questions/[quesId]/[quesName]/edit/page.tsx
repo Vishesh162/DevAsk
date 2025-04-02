64b5809bfc608c71d9c44727b908f07b90699fb1
Shashwat Vaish <91148757+Shash-04@users.noreply.github.com>
2025-04-03 01:09:14 +0530

    Update page.tsx 9

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -3,9 +3,9 @@ import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./EditQues";
 
-export default async function Page({ params }: { params: any }) {
-  // Now params is not forced to be a Promise
-  const { quesId, quesName } = params;
+export default async function Page({ params }: { params: unknown }) {
+  // Force params to be resolved as the expected object shape
+  const { quesId, quesName } = (await Promise.resolve(params)) as { quesId: string; quesName: string };
   const question = await databases.getDocument(db, questionCollection, quesId);
   return <EditQues question={question} />;
 }
